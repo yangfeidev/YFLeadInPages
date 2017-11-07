ed1edbd62a7d91dbfdf9ee45cecf8f73d33a7a82
YangFei <[yangfei_dev@sina.cn]>
2017-11-07 10:15:50 +0800

    YFTestViewController Init

---
+++ YFLeadInPages/YFAdd/YFTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  YFTestViewController.h
+//  YFLeadInPages
+//
+//  Created by YangFei on 2017/11/7.
+//  Copyright © 2017年 JiuBianLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YFTestViewController : UIViewController
+
+@end
